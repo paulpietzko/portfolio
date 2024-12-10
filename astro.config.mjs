298354f76cd90e6d3883017ee9adc1110b6c71e9
Paul Pietzko <paul.pietzko@icloud.com>
2024-12-10 19:18:58 +0100

    add @astrojs/node dependency and configure standalone adapter; update BlogPosts component for server-side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import solidJs from '@astrojs/solid-js';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
