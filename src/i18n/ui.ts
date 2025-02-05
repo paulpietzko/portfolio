529f4f3c787434e36dd4eb9d7ba909be8673817c
Paul Pietzko <paul.pietzko@icloud.com>
2025-02-05 15:53:34 +0100

    translate projects and stats section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,6 +14,10 @@ export const ui = {
     'nav.imprint': 'Imprint',
     'nav.contact': 'Get in Touch',
     'hero.text': "I'm a Frontend Developer from Switzerland with {yearsOfExperience} years of experience.",
+    'projects.title': 'Projects I work on',
+    'projects.subTitle': 'Here are some of the projects I worked on in the past.',
+    'stats.title': 'Alltime Stats',
+    'stats.subTitle': 'Here are some stats about my contributions on GitHub.',
   },
   de: {
     'nav.home': 'Start',
@@ -21,5 +25,9 @@ export const ui = {
     'nav.imprint': 'Impressum',
     'nav.contact': 'Kontakt',
     'hero.text': "Ich bin ein Frontend-Entwickler aus der Schweiz mit {yearsOfExperience} Jahren Erfahrung.",
+    'projects.title': 'Projekte, an denen ich arbeite',
+    'projects.subTitle': 'Hier sind einige der Projekte, an denen ich in der Vergangenheit gearbeitet habe.',
+    'stats.title': 'Statistiken',
+    'stats.subTitle': 'Hier sind einige Statistiken über meine Beiträge auf GitHub.',
   },
 } as const;
