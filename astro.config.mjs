9259762e0202fcbd91463d0ee2e3b49563902671
Paul Pietzko <paul.pietzko@icloud.com>
2025-04-22 21:43:24 +0200

    feat: remove i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import solidJs from '@astrojs/solid-js';
+import solidJs from "@astrojs/solid-js";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
   adapter: vercel(),
-  i18n: {
-    locales: ["en", "de"],
-    defaultLocale: "en",
-    routing: {
-        prefixDefaultLocale: true
-    }
-  }
 });
