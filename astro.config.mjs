08ec1cfb86ec970ea8dca8b048a1b75ae7ca4d64
Paul Pietzko <paul.pietzko@icloud.com>
2026-04-11 17:02:07 +0200

    feat: migrate tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 import solidJs from "@astrojs/solid-js";
-
 import vercel from "@astrojs/vercel";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.paulpietzko.com',
-  integrations: [tailwind(), solidJs(), sitemap()],
+  site: "https://www.paulpietzko.com",
+  integrations: [solidJs(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   adapter: vercel(),
 });
